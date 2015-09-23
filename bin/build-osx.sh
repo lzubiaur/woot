273d106ca1d99a90c88f7d393c0973a5a1b2be16
lzubiaur <zubiaur.laurent@gmail.com>
2015-09-23 17:35:35 +0200

    Initial commit

---
+++ bin/build-osx.sh
@@ -0,0 +1,21 @@
+if ! [ -d "build" ]
+then
+    mkdir build
+fi
+cd build
+
+if ! [ -d "build-osx" ]
+then
+    mkdir build-osx
+fi
+cd build-osx
+
+#    -DCMAKE_INSTALL_PREFIX=$libprefix \
+
+cmake -G "Xcode" \
+    -DBUILD_OSX=TRUE \
+    -DCMAKE_BUILD_TYPE=Release \
+    ../..
+
+cmake --build . --target install --config Release
+
