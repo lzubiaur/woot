13db853276e9df7c65d7e370f3ec91947d28727c
lzubiaur <zubiaur.laurent@gmail.com>
2015-09-26 00:12:08 +0200

    Add get_exec_path for linux

--- src/file_util.c
+++ src/file_util.c
@@ -1,13 +1,18 @@
 #include "file_util.h"
 
+#include <stdio.h>
 #include <stdlib.h>     /* malloc, free, realpath */
 
-#if defined(__APPLE__)
+#ifdef __APPLE__
 /* For _NSGetExecutablePath */
 #include <mach-o/dyld.h>
 #endif
 
 #ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <limits.h> /* realpath */
 #endif
 
 #ifdef _WIN32
@@ -15,16 +20,32 @@
 
 char * get_exec_path()
 {
-    uint32_t size = 0;
     char * full_path = NULL;
     char *buf = NULL;
+#ifdef __APPLE__
+    uint32_t size = 0;
     /* First call to get the buffer required size */
     _NSGetExecutablePath(NULL, &size);
     buf = (char *)malloc(size);
     /* Second call to actually get the exec path */
     _NSGetExecutablePath(buf, &size);
-    /* Get the real absolute path. realpath will allocate */
+    /* Get the real absolute path. */
     full_path = realpath(buf, NULL);
     free(buf);
+#elif defined(__linux__)
+    size_t r;
+    if ((buf = (char *)malloc(PATH_MAX)) == NULL) {
+        fprintf(stderr, "insufficient memory\n");
+	exit(EXIT_FAILURE);
+    }
+    r = readlink("/proc/self/exe",buf,PATH_MAX);
+    if (r < 0) {
+        perror("get_exec_path");
+        exit(EXIT_FAILURE);
+    }
+    buf[r] = '\0';
+    full_path = realpath(buf, NULL);
+    free(buf);
+#endif
     return full_path;
 }
