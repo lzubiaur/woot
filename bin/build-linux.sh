2d19b7bd5fd4beb52ff0f0f4fbc163bff90aa449
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-04 14:35:33 +0200

    build-linux: clean up.

--- bin/build-linux.sh
+++ bin/build-linux.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-if ! [ -d "build/linux" ]
-then
+if ! [ -d "build/linux" ]; then 
     mkdir -p "build/linux"
 fi
 cd build/linux
 
 export CXX="gcc"
 
-cmake \
+# Use default cmake
+CMAKE="cmake"
+
+$CMAKE \
     -DBUILD_LINUX=TRUE \
     -DCMAKE_BUILD_TYPE=Release \
     ../..
 
-cmake --build . --target install --config Release
+$CMAKE --build . --target install --config Release
