8d9d9a6b169f35a9af3917a15038dc82006101ce
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-14 11:30:40 +0200

    Move engine config into init.lua

---
+++ lua/engine/init.lua
@@ -0,0 +1,53 @@
+-- C module (shared library) search path
+package.cpath = '../MacOS/lib?.dylib;./bin/lib?.so;./bin/?.dll'
+
+require 'engine.fileutil'.init()
+
+-- =============================================================================
+-- Module loading
+
+-- Load and open the LuaFileSystem
+-- Note that C module can be loaded using `package.loadlib` instead of `require`
+-- but loadlib does not perform any path searching unlike require.
+
+-- Low level C module loading
+-- local path = "../MacOS/liblfs.dylib"
+-- local f = assert(package.loadlib('lfs', "luaopen_lfs"))
+-- f()  -- actually open the library
+
+-- Load Lua File System using `require`
+local lfs = require 'lfs'
+print(lfs.currentdir())
+
+-- GLFW shared library is libglfw.so on OSX and Linux but it's named glfw3.dll on Windows
+local glfw = require 'engine.glfw'(jit.os == 'Windows' and 'glfw3' or 'glfw')
+
+-- Initialize the library
+if glfw.Init() == 0 then
+  return
+end
+
+-- Create a windowed mode window and its OpenGL context
+local window = glfw.CreateWindow(640, 480, "Hello World")
+if window == 0 then
+  glfw.Terminate()
+  return
+end
+
+local debugWindow = glfw.CreateWindow(200,480, "Debug")
+
+-- Make the window's context current
+glfw.MakeContextCurrent(window)
+
+-- Loop until the user closes the window
+while glfw.WindowShouldClose(window) == 0 do
+  -- Render here
+
+  -- Swap front and back buffers
+  glfw.SwapBuffers(window)
+
+  -- Poll for and process events
+  glfw.PollEvents()
+end
+
+glfw.Terminate()
--- lua/main.lua
+++ lua/main.lua
@@ -1,53 +1,5 @@
--- Lua module search path
+-- Set the Lua module search path
 package.path = './lua/?.lua'
--- C module (shared library) search path
-package.cpath = '../MacOS/lib?.dylib;./bin/lib?.so;./bin/?.dll'
 
-assert(jit.os == 'OSX' or jit.os == 'Linux' or jit.os == 'Windows', 'Target OS not supported: ' .. jit.os)
-require 'engine.fileutil'.init()
-
--- =============================================================================
--- Module loading
-
--- Load and open the LuaFileSystem
--- Note that C module can be loaded using `package.loadlib` instead of `require`
--- but loadlib does not perform any path searching unlike require.
-
--- Low level C module loading
--- local path = "../MacOS/liblfs.dylib"
--- local f = assert(package.loadlib('lfs', "luaopen_lfs"))
--- f()  -- actually open the library
-
--- Load Lua File System using `require`
-local lfs = require 'lfs'
-print(lfs.currentdir())
-
-local glfw = require 'engine.glfw'(jit.os == 'Windows' and 'glfw3' or 'glfw')
-
--- Initialize the library
-if glfw.Init() == 0 then
-  return
-end
-
--- Create a windowed mode window and its OpenGL context
-local window = glfw.CreateWindow(640, 480, "Hello World")
-if window == 0 then
-  glfw.Terminate()
-  return
-end
-
--- Make the window's context current
-glfw.MakeContextCurrent(window)
-
--- Loop until the user closes the window
-while glfw.WindowShouldClose(window) == 0 do
-  -- Render here
-
-  -- Swap front and back buffers
-  glfw.SwapBuffers(window)
-
-  -- Poll for and process events
-  glfw.PollEvents()
-end
-
-glfw.Terminate()
+-- Init the engine
+require 'engine.init'
