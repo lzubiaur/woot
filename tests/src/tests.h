3a7efe49f0a15249aff4af4da4125b24feb39ebf
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-18 20:02:48 +0200

    Add tests header with extern c

---
+++ tests/src/tests.h
@@ -0,0 +1,23 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function MUST have 'C' linkage (compiler does not mangle the name). If not
+ * declared using extern "C" then C++ function (e.g. imgui_test_render in imgui.cpp)
+ * will be decorated and Lua FFI binding will not work.
+ * To check that a function has no decoration use the `nm` command.
+ * For instance, when no declared using extern "C" the imgui_test_render is exported
+ * as `__Z17imgui_test_renderv` in the shared library. When using C linkage it has the
+ * symbole `_imgui_test_render`.
+ */
+void test_1();
+void imgui_test_render();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_H */
