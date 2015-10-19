5b008aa1dc4f27090794806d4c9cc5da2ba29971
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-19 14:09:53 +0200

    Fix ImGui requirements

--- lua/engine/viewport.lua
+++ lua/engine/viewport.lua
@@ -4,6 +4,8 @@ local Viewport = {}
 local name = (jit.os == 'Windows') and 'glfw' or 'glfw.3'
 local glfw = require 'engine.glfw'(name)
 
+local jit = require 'jit'
+
 local scene_tree = {}
 local window = nil
 
@@ -15,12 +17,28 @@ function Viewport.init()
 end
 
 function Viewport.create(width, height, windowName)
-    window = glfw.CreateWindow(width, height, windowName)
-    Viewport.window = window
-    if window == 0 then
-      glfw.Terminate()
-      return false
+    -- specify the client API version that the created context must be compatible with
+    -- glfwCreateWindow will still fail if the resulting OpenGL version is less than the one requested
+    -- ImGui requires OpenGL 3.3
+    glfw.WindowHint('CONTEXT_VERSION_MAJOR', 3);
+    glfw.WindowHint('CONTEXT_VERSION_MINOR', 3);
+    -- specifies which OpenGL profile to create the context for:
+    -- OPENGL_CORE_PROFILE or OPENGL_COMPAT_PROFILE, or OPENGL_ANY_PROFILE.
+    -- If requesting an OpenGL version below 3.2, OPENGL_ANY_PROFILE must be used
+    glfw.WindowHint('OPENGL_PROFILE', 'OPENGL_CORE_PROFILE');
+    if (jit.os == 'OSX') then
+        -- OpenGL context should be forward-compatible (all functionality deprecated in the requested version of OpenGL is removed)
+        -- This may only be used if the requested OpenGL version is 3.0 or above.
+        glfw.WindowHint('OPENGL_FORWARD_COMPAT', 1);
     end
+    -- Resizable window in non fullscreen mode
+    -- glfw.WindowHint('RESIZABLE',1)
+
+    window = glfw.CreateWindow(width, height, windowName)
+    if window == nil then
+      glfw.Terminate()
+    end
+    return window
 end
 
 function Viewport.addNode(node)
