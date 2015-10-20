ed36e130b79b38eece8b3e90b9a2c3c94c6f0152
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-20 08:30:42 +0200

    Remove binding test

--- tests/lua/tests/imgui.lua
+++ tests/lua/tests/imgui.lua
@@ -5,26 +5,23 @@ local fileutil = require 'engine.fileutil'
 local viewport = require 'engine.viewport'
 
 ffi.cdef [[
-    void test_1();
     void imgui_test_render();
 ]]
 
--- Test binding
-local tests_bind = ffi.load(fileutil.getModulePath('tests'))
-tests_bind.test_1()
+local tests = ffi.load(fileutil.getModulePath('tests'))
 
 ffi.cdef [[
     struct GLFWwindow;
     bool ImGui_ImplGlfwGL3_Init(GLFWwindow* window, bool install_callbacks);
 ]]
-local imgui_bind = ffi.load(fileutil.getModulePath('imgui'))
+local imgui = ffi.load(fileutil.getModulePath('imgui'))
 
-imgui_bind.ImGui_ImplGlfwGL3_Init(viewport.getWindow(), true)
+imgui.ImGui_ImplGlfwGL3_Init(viewport.getWindow(), true)
 
 return {
     process = function(delta)
     end,
     render = function()
-        tests_bind.imgui_test_render()
+        tests.imgui_test_render()
     end
 }
