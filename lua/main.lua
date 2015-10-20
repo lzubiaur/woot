e4a71fd1111390cbc2bb7fbb1badf0a44f77cebb
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-20 07:41:03 +0200

    Clean up and add comments

--- lua/main.lua
+++ lua/main.lua
@@ -1,7 +1,12 @@
 -- Set the Lua module search path
 package.path = './lua/?.lua'
-
+-- Load the game engine
 local viewport = require 'engine.viewport'
-viewport.create(1280, 720, 'test')
-viewport.addNode(require 'imgui')
+-- Test module loading and binding. Must be called after the engine is loaded (require 'engine.viewport')
+require 'tests.mod'
+-- Create the viewport
+viewport.create(1280, 720, 'Woot Game Egine')
+-- Load the GUI test
+viewport.addNode(require 'tests.imgui')
+-- Run the tests
 viewport.run()
