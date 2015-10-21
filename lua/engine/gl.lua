ac7f3982aff51cfcfc006b8f8f315843d900b366
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-21 14:19:54 +0200

    Move OpenGL declaration in gl_h.lua

--- lua/engine/gl.lua
+++ lua/engine/gl.lua
@@ -1,17 +1,7 @@
+local fileutil = require 'engine.fileutil'
 local ffi = require 'ffi'
 
-ffi.cdef [[
-enum {
-    GL_DEPTH_BUFFER_BIT               = 0x00000100,
-    GL_STENCIL_BUFFER_BIT             = 0x00000400,
-    GL_COLOR_BUFFER_BIT               = 0x00004000
-};
-    typedef float GLclampf;
-    typedef int GLint;
-    typedef int GLsizei;
-    typedef unsigned int GLbitfield;
-    void glClear (GLbitfield mask);
-    void glViewport (GLint x, GLint y, GLsizei width, GLsizei height);
-    void glClearColor (GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
-]]
-return ffi.load(ffi.os == 'OSX' and 'OpenGL.framework/OpenGL' or 'GL')
+require 'engine.gl_h'
+
+-- return ffi.load(ffi.os == 'OSX' and 'OpenGL.framework/OpenGL' or 'GL')
+return ffi.load(fileutil.getModulePath('gl3w'))
