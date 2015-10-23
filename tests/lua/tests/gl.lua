0120745cd6257b3c7b6b30075223b3a7956ad317
lzubiaur <zubiaur.laurent@gmail.com>
2015-10-23 08:57:59 +0200

    Fix wrong vao and vbo number

--- tests/lua/tests/gl.lua
+++ tests/lua/tests/gl.lua
@@ -7,11 +7,8 @@ local program, posAttrib = nil, nil
 
 function _render()
     gl.glUseProgram(program)
-    gl.glVertexAttribPointer(posAttrib, 2, gl.GL_FLOAT, gl.GL_FALSE, 0, nil)
-    gl.glEnableVertexAttribArray(posAttrib)
     gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
 end
-
 
 function _init()
     local vertexSource = [[
@@ -32,20 +29,22 @@ function _init()
         }
     ]]
 
+    local vao = ffi.new 'GLuint[1]'
+    local vbo = ffi.new 'GLuint[1]'
     local vertices = ffi.new('float[6]', { 0.0, 0.5, 0.5, -0.5, -0.5, -0.5} )
 
-    local vbo = ffi.new 'GLuint[1]'
     gl.glGenBuffers(1, vbo)
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo[1])
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo[0])
     gl.glBufferData(gl.GL_ARRAY_BUFFER, ffi.sizeof(vertices), vertices, gl.GL_STATIC_DRAW);
 
     program = gl.createProgram(vertexSource, fragmentSource)
 
-    posAttrib = gl.glGetAttribLocation(program, 'position')
-
-    local vao = ffi.new 'GLuint[1]'
     gl.glGenVertexArrays(1, vao);
-    gl.glBindVertexArray(vao[1]);
+    gl.glBindVertexArray(vao[0]);
+
+    posAttrib = gl.glGetAttribLocation(program, 'position')
+    gl.glVertexAttribPointer(posAttrib, 2, gl.GL_FLOAT, gl.GL_FALSE, 0, nil)
+    gl.glEnableVertexAttribArray(posAttrib)
 
     render_test = _render
 end
